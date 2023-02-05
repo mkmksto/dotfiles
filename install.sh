d96187077c0a44cebfae0e9944872d06dd905769
mkmksto <119323664+mkmksto@users.noreply.github.com>
2023-02-06 00:19:14 +0800

    added .gitconfig to symlink script

--- install.sh
+++ install.sh
@@ -1,9 +1,11 @@
 rm ~/.bashrc
 rm ~/.dircolors
+rm ~/.gitconfig
 rm ~/.p10k.zsh
 rm ~/.zshrc
 
 ln -s ~/dotfiles/.bashrc ~/.bashrc
 ln -s ~/dotfiles/.dircolors ~/.dircolors
+ln -s ~/dotfiles/.gitconfig ~/.gitconfig
 ln -s ~/dotfiles/.p10k.zsh ~/.p10k.zsh
 ln -s ~/dotfiles/.zshrc ~/.zshrc
